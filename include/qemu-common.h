1ab1556d79d4e251012df10b948c36ad53d988f6
Sean Bruno <sbruno@freebsd.org>
2014-06-01 19:22:06 -0700

    Fix build on ppc.  Take from emulators/qemu-devel/files/patch-include-qemu-common.h

--- include/qemu-common.h
+++ include/qemu-common.h
@@ -434,7 +434,9 @@ void qemu_hexdump(const char *buf, FILE *fp, const char *prefix, size_t size);
 #define ALL_EQ(v1, v2) vec_all_eq(v1, v2)
 /* altivec.h may redefine the bool macro as vector type.
  * Reset it to POSIX semantics. */
+#ifndef __cplusplus
 #define bool _Bool
+#endif
 #elif defined __SSE2__
 #include <emmintrin.h>
 #define VECTYPE        __m128i
