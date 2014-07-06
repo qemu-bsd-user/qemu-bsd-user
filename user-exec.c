73d16f7f79254e12e9610257e01e15075f882085
Sean Bruno <sbruno@freebsd.org>
2014-07-06 10:49:08 -0700

    Writing to readonly page can cause trap 0xc on FreeBSD too.
    Submitted-by: Juergen Lock <nox@jelal.kn-bremen.de>

--- user-exec.c
+++ user-exec.c
@@ -231,7 +231,12 @@ int cpu_signal_handler(int host_signum, void *pinfo,
 
     pc = PC_sig(uc);
     return handle_cpu_signal(pc, (unsigned long)info->si_addr,
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+                             (TRAP_sig(uc) == 0xe ||
+                              TRAP_sig(uc) == 0xc) ?
+#else
                              TRAP_sig(uc) == 0xe ?
+#endif
                              (ERROR_sig(uc) >> 1) & 1 : 0,
                              &MASK_sig(uc), puc);
 }
