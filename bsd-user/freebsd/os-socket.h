687136b813c0e3bd8de32c861ab902d3ca0e67c9
Sean Bruno <sbruno@chips.ysv.freebsd.org>
2014-10-04 21:43:09 +0000

    Fixup white space and tabs.

--- bsd-user/freebsd/os-socket.h
+++ bsd-user/freebsd/os-socket.h
@@ -59,9 +59,9 @@ static inline abi_long do_freebsd_sendmsg(int fd, abi_ulong target_msg,
         msg.msg_control = alloca(msg.msg_controllen);
         msg.msg_flags = tswap32(msgp->msg_flags);
     } else {
-	msg.msg_controllen = 0;
-	msg.msg_control = NULL;
-	msg.msg_flags = 0;
+        msg.msg_controllen = 0;
+        msg.msg_control = NULL;
+        msg.msg_flags = 0;
     }
 
     count = tswapal(msgp->msg_iovlen);
@@ -72,9 +72,9 @@ static inline abi_long do_freebsd_sendmsg(int fd, abi_ulong target_msg,
     msg.msg_iov = vec;
 
     if (msg.msg_controllen > 0)
-    	ret = t2h_freebsd_cmsg(&msg, msgp);
+        ret = t2h_freebsd_cmsg(&msg, msgp);
     else /* no ancillary data */
-	ret = 0;
+        ret = 0;
     if (!is_error(ret)) {
         ret = get_errno(sendmsg(fd, &msg, flags));
     }
@@ -125,10 +125,10 @@ static inline abi_long do_freebsd_recvmsg(int fd, abi_ulong target_msg,
     ret = get_errno(recvmsg(fd, &msg, flags));
     if (!is_error(ret)) {
         len = ret;
-	if (msg.msg_controllen > 0)
-            ret = h2t_freebsd_cmsg(msgp, &msg);
-	else /* no ancillary data */
-            ret = 0;
+    if (msg.msg_controllen > 0)
+        ret = h2t_freebsd_cmsg(msgp, &msg);
+    else /* no ancillary data */
+        ret = 0;
         if (!is_error(ret)) {
             msgp->msg_namelen = tswap32(msg.msg_namelen);
             if (msg.msg_name != NULL) {
