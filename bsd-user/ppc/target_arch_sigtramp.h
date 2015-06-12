4670e51380ec952a3d678ab6e8fda2cb9c6377ec
Justin Hibbits <chmeeedalf@gmail.com>
2015-06-12 13:29:21 -0700

    Fix offset in signal trampoline for powerpc.
    The trampoline adjusts the stack pointer for private data, but wasn't factoring
    that into the argument to pass to sigreturn().

--- bsd-user/ppc/target_arch_sigtramp.h
+++ bsd-user/ppc/target_arch_sigtramp.h
@@ -9,13 +9,13 @@ static inline abi_long setup_sigtramp(abi_ulong offset, unsigned sigf_uc,
     int i;
 #if defined(TARGET_PPC64) && !defined(TARGET_ABI32)
     uint32_t sigtramp_code[TARGET_SZSIGCODE/TARGET_INSN_SIZE] = {
-    /*  1 */ 0x3821FFF0,   /* addi r1,r1,-48 */
+    /*  1 */ 0x3821FF90,   /* addi r1,r1,-112 */
     /*  2 */ 0x7C4802A6,   /* mflr r2 */
     /*  3 */ 0xE8020000,   /* ld r0,0(r2) */
     /*  4 */ 0xE8420008,   /* ld r2,8(r2) */
     /*  5 */ 0x7C0803A6,   /* mtlr r0 */
     /*  6 */ 0x4E800021,   /* blrl */
-    /*  7 */ 0x38610000 + sigf_uc,   /* addi r3,r1,48 */
+    /*  7 */ 0x38610070 + sigf_uc,   /* addi r3,r1,112+SF_UC */
     /*  8 */ 0x38000000 + sys_sigreturn, /* li r0,SYS_sigreturn */
     /*  9 */ 0x44000002,   /* sc */
     /* 10 */ 0x38000001,   /* li r0,1 */
@@ -24,9 +24,9 @@ static inline abi_long setup_sigtramp(abi_ulong offset, unsigned sigf_uc,
     };
 #else
     uint32_t sigtramp_code[TARGET_SZSIGCODE/TARGET_INSN_SIZE] = {
-    /* 1 */ 0x3821FFF0,   /* addi r1,r1,-16 */
+    /* 1 */ 0x3821FFEC,   /* addi r1,r1,-20 */
     /* 2 */ 0x4E800021,   /* blrl */
-    /* 3 */ 0x38610000 + sigf_uc,   /* addi r3,r1,16 */
+    /* 3 */ 0x38610014 + sigf_uc,   /* addi r3,r1,20+SF_UC */
     /* 4 */ 0x38000000 + sys_sigreturn, /* li r0,SYS_sigreturn */
     /* 5 */ 0x44000002,   /* sc */
     /* 6 */ 0x38000001,   /* li r0,1 */
