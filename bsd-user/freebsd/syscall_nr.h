73bb4f5ace3f446fbb5bb1b7c1de0de0a4e2f39a
Jung-uk Kim <jkim@FreeBSD.org>
2017-03-01 17:46:50 -0500

    Reduce white space changes from make_syscall_nr_h.sh generated file.

--- bsd-user/freebsd/syscall_nr.h
+++ bsd-user/freebsd/syscall_nr.h
@@ -466,10 +466,9 @@
 #define	TARGET_FREEBSD_NR_pipe2	542
 #define	TARGET_FREEBSD_NR_aio_mlock	543
 #define	TARGET_FREEBSD_NR_procctl	544
-#define	TARGET_FREEBSD_NR_ppoll		545
+#define	TARGET_FREEBSD_NR_ppoll	545
 #define	TARGET_FREEBSD_NR_futimens	546
 #define	TARGET_FREEBSD_NR_utimensat	547
-
 #define	TARGET_FREEBSD_NR_MAXSYSCALL	548
 /* Legacy system calls. */
 #ifndef	TARGET_FREEBSD_NR_killpg
