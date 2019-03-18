6b49a5dd59d2c60391543192b0c0f13fa6b6121b
Colin Percival <cperciva@tarsnap.com>
2019-03-18 11:59:53 -0700

    Add '-0 argv0' option to bsd-user/main.c
    Previously it was impossible to emulate a program with a file name
    different from its argv[0].  With this change, you can run
      qemu -0 fakename realname args
    which runs the program "realname" with an argv of "fakename args".

--- bsd-user/main.c
+++ bsd-user/main.c
@@ -256,6 +256,7 @@ int main(int argc, char **argv)
     envlist_t *envlist = NULL;
     char *trace_file = NULL;
     bsd_type = HOST_DEFAULT_BSD_TYPE;
+    char * argv0 = NULL;
 
     if (argc <= 1)
         usage();
@@ -368,6 +369,8 @@ int main(int argc, char **argv)
         } else if (!strcmp(r, "trace")) {
             g_free(trace_file);
             trace_file = trace_opt_parse(optarg);
+        } else if (!strcmp(r, "0")) {
+            argv0 = argv[optind++];    
         } else {
             usage();
         }
@@ -393,6 +396,8 @@ int main(int argc, char **argv)
         usage();
     }
     filename = argv[optind];
+    if (argv0)
+        argv[optind] = argv0;
 
     if (!trace_init_backends()) {
         exit(1);
