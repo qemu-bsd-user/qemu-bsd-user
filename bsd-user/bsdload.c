qemu-bsd-user/qemu-bsd-user

commit 634f539a7ec6a8393eac41dba550246087c36259

bsd-user: fix and simplify loader_exec()
Consider the case where one is inside an armv7 jail, and attempts to
execute a binary from PATH:

$ xz

One's shell does a PATH search and locates /usr/bin/xz, as it should,
then attempts to execute /usr/bin/xz with argv[0] == "xz".

If /usr/bin/xz is a target binary, imgact_binmisc picks it up and
executes qemu-arm-static with argv[1] == "xz", which then does its
own PATH search (while this is perhaps avoidable, it should generally
work).

Cue loader_exec(), which resolves argv[0] to a binary. The bug here was
that it would *always* realpath(argv[0]) if PATH was set, meaning that
if we had a directory entry in cwd with the same name, we'd find that
instead and execute it if it were a target binary or fail if it were
not executable.

This change reworks the logic entirely to do something more closely
approximating the correct thing. If we find a slash in argv[0], we'll
just try it as-is or fail on its own merits if it's not executable. If
not, then we'll try a PATH search with the argv[0] that we were passed.

The return values of is_there() and find_in_path() have been converted
to bools just to make the semantics easier to guess at, and there is no
reason for find_in_path() to be a tri-value function. The non-search
functionality has been ripped out of find_in_path() for the time being,
because this is the only use for it and the name does clearly indicate
that it will search a path.

Reported-by: Robert Clausecker <fuz@fuz.su>
Fixes: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=253375

bsd-user/bsdload.c
@@ -141,7 +141,7 @@ abi_ulong loader_build_argptr(int envc, int argc, abi_ulong sp,
     return sp;
 }
 
-static int is_there(const char *candidate)
+static bool is_there(const char *candidate)
 {
     struct stat fin;
 
@@ -149,30 +149,17 @@ static int is_there(const char *candidate)
     if (access(candidate, X_OK) == 0 && stat(candidate, &fin) == 0 &&
             S_ISREG(fin.st_mode) && (getuid() != 0 ||
                 (fin.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) != 0)) {
-        return 1;
+        return true;
     }
 
-    return 0;
+    return false;
 }
 
-static int find_in_path(char *path, const char *filename, char *retpath,
+static bool find_in_path(char *path, const char *filename, char *retpath,
         size_t rpsize)
 {
     const char *d;
-    int found;
 
-    if (strchr(filename, '/') != NULL) {
-        if (is_there(filename)) {
-                if (!realpath(filename, retpath)) {
-                    return -1;
-                }
-                return 0;
-        } else {
-            return -1;
-        }
-    }
-
-    found = 0;
     while ((d = strsep(&path, ":")) != NULL) {
         if (*d == '\0') {
             d = ".";
@@ -181,11 +168,10 @@ static int find_in_path(char *path, const char *filename, char *retpath,
             continue;
         }
         if (is_there((const char *)retpath)) {
-            found = 1;
-            break;
+            return true;
         }
     }
-    return found;
+    return false;
 }
 
 int loader_exec(const char *filename, char **argv, char **envp,
@@ -193,45 +179,39 @@ int loader_exec(const char *filename, char **argv, char **envp,
              struct bsd_binprm *bprm)
 {
     char *p, *path = NULL, fullpath[PATH_MAX];
-    const char *execname = NULL;
-    int retval, i, found;
+    int retval, i;
 
     bprm->p = TARGET_PAGE_SIZE * MAX_ARG_PAGES; /* -sizeof(unsigned int); */
     for (i=0 ; i<MAX_ARG_PAGES ; i++)       /* clear page-table */
             bprm->page[i] = NULL;
 
-    /* Find target executable in path, if not already an absolute path. */
-    p = getenv("PATH");
-    if (p != NULL) {
+    if (strchr(filename, '/') != NULL) {
+        if (!is_there(filename)) {
+            return -1;
+        }
+        retval = open(filename, O_RDONLY);
+        bprm->fullpath = NULL;
+    } else {
+        p = getenv("PATH");
+        if (p == NULL) {
+            return -1;
+        }
+
         path = g_strdup(p);
         if (path == NULL) {
             fprintf(stderr, "Out of memory\n");
             return -1;
         }
-        execname = realpath(filename, NULL);
-        if (execname == NULL) {
-            execname = g_strdup(filename);
-        }
-        found = find_in_path(path, execname, fullpath, sizeof(fullpath));
-        /* Absolute path specified but not found? */
-        if (found == -1) {
+
+        if (!find_in_path(path, filename, fullpath, sizeof(fullpath))) {
             return -1;
         }
-        if (found) {
-            retval = open(fullpath, O_RDONLY);
-            bprm->fullpath = g_strdup(fullpath);
-        } else {
-            retval = open(execname, O_RDONLY);
-            bprm->fullpath = g_strdup(execname);
-        }
-        if (execname) {
-            g_free((void *)execname);
-        }
+        retval = open(fullpath, O_RDONLY);
+        bprm->fullpath = g_strdup(fullpath);
+
         g_free(path);
-    } else {
-        retval = open(filename, O_RDONLY);
-        bprm->fullpath = NULL;
     }
+
     if (retval < 0) {
         return retval;
     }

commit 1e58513a703965ea0c59e822f640fc9c7485e3c6

bsd-user: bsdload: ensure bprm->fullpath is always set
Refactoring in the area assumed that bprm->fullpath could be NULL, but
it turns out that that is not the case -- various parts assume that
it is not NULL, so let's make sure we fully resolve it in all cases.

The assumption of it not being NULL that was initially tripped over was
in do_bsd_readlink, but I note that there are other parts of
bsd-user/freebsd that also reference it in questionable ways.

This commit resolves the filename passed in for contexts where a path is
specified (thus, no PATH search), and ensures that bprm->fullpath is
documented as required and populated (with error as needed) early on.

Fixes: 634f539a7e

bsd-user/bsdload.c
@@ -186,11 +186,16 @@ int loader_exec(const char *filename, char **argv, char **envp,
             bprm->page[i] = NULL;
 
     if (strchr(filename, '/') != NULL) {
-        if (!is_there(filename)) {
+        path = realpath(filename, fullpath);
+        if (path == NULL) {
+            /* Failed to resolve. */
             return -1;
         }
-        retval = open(filename, O_RDONLY);
-        bprm->fullpath = NULL;
+        if (!is_there(path)) {
+            return -1;
+        }
+        retval = open(path, O_RDONLY);
+        bprm->fullpath = g_strdup(path);
     } else {
         p = getenv("PATH");
         if (p == NULL) {
@@ -212,6 +217,11 @@ int loader_exec(const char *filename, char **argv, char **envp,
         g_free(path);
     }
 
+    /* bprm->fullpath must be populated. */
+    if (bprm->fullpath == NULL) {
+        fprintf(stderr, "Out of memory\n");
+        return -1;
+    }
     if (retval < 0) {
         return retval;
     }