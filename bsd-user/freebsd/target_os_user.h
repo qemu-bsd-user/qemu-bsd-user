8f8ef4f77bd7c36e22723e0bd0818ff00271d12d
Warner Losh <imp@FreeBSD.org>
2021-04-30 13:00:54 -0600

    Catch up with FreeBSD ecfbddf0cde3
    The kinfo_vmentry struct grew a union. Catch up to that growth. So far
    this is only in -current and 1400009 represents 29 commits after this
    change.

--- bsd-user/freebsd/target_os_user.h
+++ bsd-user/freebsd/target_os_user.h
@@ -402,7 +402,16 @@ struct target_kinfo_vmentry {
 	uint16_t kve_vn_mode;			/* File mode. */
 	uint16_t kve_status;			/* Status flags. */
 #if defined(__FreeBSD_version) && __FreeBSD_version >= 1200031
+#if __FreeBSD_version >= 1400009
+	union {
+		uint64_t _kve_vn_fsid;		/* dev_t of vnode location */
+		uint64_t _kve_obj;		/* handle of anon obj */
+	} kve_type_spec;
+#define	kve_vn_fsid	kve_type_spec._kve_vn_fsid
+#define	kve_obj		kve_type_spec._kve_obj
+#else
 	uint64_t kve_vn_fsid;			/* dev_t of vnode location */
+#endif
 	uint64_t kve_vn_rdev;			/* Device id if device. */
 	int	 _kve_ispare[8];		/* Space for more stuff. */
 #else
