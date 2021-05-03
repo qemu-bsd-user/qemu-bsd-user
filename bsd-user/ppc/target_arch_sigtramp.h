02d7237a1785e7afca9b8ec54d7821a54f0710b3
Brandon Bergren <bdragon@FreeBSD.org>
2021-05-02 19:26:09 -0500

    bsd-user: Fix ELFv2 signal trampolining.
    This brings in updates from the FreeBSD trampoline that allow for dual-ABI
    with a single handler. The only caveat is that we need to change the
    entry point calculation for ELFv2 to skip over the first four instructions.
    
    Signed-off-by: Brandon Bergren <bdragon@FreeBSD.org>

--- bsd-user/ppc/target_arch_sigtramp.h
+++ bsd-user/ppc/target_arch_sigtramp.h
@@ -9,18 +9,21 @@ static inline abi_long setup_sigtramp(abi_ulong offset, unsigned sigf_uc,
     int i;
 #if defined(TARGET_PPC64) && !defined(TARGET_ABI32)
     uint32_t sigtramp_code[TARGET_SZSIGCODE/TARGET_INSN_SIZE] = {
-    /*  1 */ 0x3821FF90,   /* addi r1,r1,-112 */
-    /*  2 */ 0x7C4802A6,   /* mflr r2 */
-    /*  3 */ 0xE8020000,   /* ld r0,0(r2) */
-    /*  4 */ 0xE8420008,   /* ld r2,8(r2) */
-    /*  5 */ 0x7C0803A6,   /* mtlr r0 */
-    /*  6 */ 0x4E800021,   /* blrl */
-    /*  7 */ 0x38610070 + sigf_uc,   /* addi r3,r1,112+SF_UC */
-    /*  8 */ 0x38000000 + sys_sigreturn, /* li r0,SYS_sigreturn */
-    /*  9 */ 0x44000002,   /* sc */
-    /* 10 */ 0x38000001,   /* li r0,SYS_exit */
-    /* 11 */ 0x44000002,   /* sc */
-    /* 12 */ 0x60000000    /* nop */
+    /* ELFv1 entry point */
+    /*  1 */ 0x7C4802A6,   /* mflr r2 */
+    /*  2 */ 0xE8020000,   /* ld r0,0(r2) */
+    /*  3 */ 0xE8420008,   /* ld r2,8(r2) */
+    /*  4 */ 0x7C0803A6,   /* mtlr r0 */
+    /* ELFv2 entry point */
+    /*  5 */ 0x3821FF90,   /* addi r1,r1,-112 */
+    /* Needed for ELFv2 ABI compliance */
+    /*  6 */ 0x7D8802A6,   /* mflr r12 */
+    /*  7 */ 0x4E800021,   /* blrl */
+    /*  8 */ 0x38610070 + sigf_uc,   /* addi r3,r1,112+SF_UC */
+    /*  9 */ 0x38000000 + sys_sigreturn, /* li r0,SYS_sigreturn */
+    /* 10 */ 0x44000002,   /* sc */
+    /* 11 */ 0x38000001,   /* li r0,SYS_exit */
+    /* 12 */ 0x44000002    /* sc */
     };
 #else
     uint32_t sigtramp_code[TARGET_SZSIGCODE/TARGET_INSN_SIZE] = {
