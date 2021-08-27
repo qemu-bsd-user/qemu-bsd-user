17aa767ce22759e5bda66e8a8694b5ea8a0d5598
Warner Losh <imp@FreeBSD.org>
2021-08-27 14:47:44 -0600

    bsd-user: fix style issues with comments in fork_end()

--- bsd-user/main.c
+++ bsd-user/main.c
@@ -117,8 +117,8 @@ void fork_end(int child)
     if (child) {
         CPUState *cpu, *next_cpu;
         /*
-         * Child processes created by fork() only have a single thread.
-         * Discard information about the parent threads.
+         * Child processes created by fork() only have a single thread.  Discard
+         * information about the parent threads.
          */
         CPU_FOREACH_SAFE(cpu, next_cpu) {
             if (cpu != thread_cpu) {
@@ -126,8 +126,9 @@ void fork_end(int child)
             }
         }
         mmap_fork_end(child);
-        /* qemu_init_cpu_list() takes care of reinitializing the
-         * exclusive state, so we don't need to end_exclusive() here.
+        /*
+         * qemu_init_cpu_list() takes care of reinitializing the exclusive
+         * state, so we don't need to end_exclusive() here.
          */
         qemu_init_cpu_list();
         gdbserver_fork(thread_cpu);
