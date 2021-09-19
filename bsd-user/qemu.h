80a4ecf85e71b2cd0ad00948db785b8624a9dc5e
Warner Losh <imp@bsdimp.com>
2021-09-18 22:02:09 -0600

    bsd-user: remove used from TaskState
    The used field is write-only, so remove it.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/qemu.h
+++ bsd-user/qemu.h
@@ -108,7 +108,6 @@ typedef struct TaskState {
     pid_t ts_tid;     /* tid (or pid) of this task */
 
     struct TaskState *next;
-    int used; /* non zero if used */
     struct image_info *info;
     struct bsd_binprm *bprm;
 
