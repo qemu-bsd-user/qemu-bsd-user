3e936e921fb8ac4ef30a7ab2c02f66f93d14dbc6
Warner Losh <imp@bsdimp.com>
2021-09-19 10:24:10 -0600

    bsd-user/qemu-os.h: conversion routines for data structures used in os-*
    Signed-off-by: Warner Losh <imp@bsdimp.com>

---
+++ bsd-user/freebsd/qemu-os.h
@@ -0,0 +1,26 @@
+/*
+ *  FreeBSD conversion extern declarations
+ *
+ *  Copyright (c) 2013 Stacey D. Son
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _QEMU_OS_H_
+#define _QEMU_OS_H_
+
+/* os-time.c */
+int host_to_target_timerid(int timerid);
+
+#endif /* !_QEMU_OS_H_ */
