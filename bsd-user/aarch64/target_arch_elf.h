3ab01aaade572d8a6b05d270e5179c620ad2cadf
Warner Losh <imp@bsdimp.com>
2021-09-20 14:03:28 -0600

    bsd-user/target_arch_elf.h: Fix bad copyright
    Remove incomplete email from copyright.
    
    Signed-Off-By: Warner Losh <imp@bsdimp.com>

--- bsd-user/aarch64/target_arch_elf.h
+++ bsd-user/aarch64/target_arch_elf.h
@@ -1,7 +1,7 @@
 /*
  * ARM AArch64 ELF definitions for bsd-user
  *
- * Copyright (c) 2015 Stacey D. Son <sson at Freebsd>
+ * Copyright (c) 2015 Stacey D. Son
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
