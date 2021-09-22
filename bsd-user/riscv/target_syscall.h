22e0847871a9db393b8d2fb73769490873ed7ef9
Warner Losh <imp@bsdimp.com>
2021-09-22 14:51:20 -0600

    bsd-user/riscv: Mark Corbin is no longer at this address
    Signed-off-by: Mark Corbin <mark@dibsco.co.uk>
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/riscv/target_syscall.h
+++ bsd-user/riscv/target_syscall.h
@@ -1,7 +1,7 @@
 /*
  *  RISC-V system call definitions
  *
- *  Copyright (c) Mark Corbin <mark.corbin@embecosm.com>
+ *  Copyright (c) Mark Corbin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
