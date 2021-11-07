f036badc10f162aea5af9464fd366c6bce48833e
Warner Losh <imp@bsdimp.com>
2021-11-07 11:21:22 -0700

    bsd-user/host/arm/hostdep.h: Define ADJUST_SYSCALL_RETCODE
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/host/arm/hostdep.h
+++ bsd-user/host/arm/hostdep.h
@@ -15,6 +15,11 @@
 /* We have a safe-syscall.inc.S */
 #define HAVE_SAFE_SYSCALL
 
+#define ADJUST_SYSCALL_RETCODE \
+    bcc 2f;                    \
+    neg r0, r0;                \
+    2:
+
 #ifndef __ASSEMBLER__
 
 /* These are defined by the safe-syscall.inc.S file */
