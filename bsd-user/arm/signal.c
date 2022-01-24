b6a8fe552bfda3a2579f597ff12124091940f8d2
Warner Losh <imp@bsdimp.com>
2022-01-24 01:29:53 -0700

    bsd-user/arm/signal.c: get_mcontext should zero vfp data
    FreeBSD's get_mcontext doesn't return any vfp data. Instead, it zeros
    out the vfp feilds (and all the spare fields). Impelement this
    behavior. We're still missing the sysarch(ARM_GET_VFPCONTEXT) syscall,
    though.
    
    Sponsored by:		Netflix

--- bsd-user/arm/signal.c
+++ bsd-user/arm/signal.c
@@ -109,6 +109,14 @@ abi_long get_mcontext(CPUARMState *env, target_mcontext_t *mcp, int flags)
     gr[TARGET_REG_LR] = tswap32(env->regs[14]);
     gr[TARGET_REG_PC] = tswap32(env->regs[15]);
 
+    /*
+     * FreeBSD's set_mcontext doesn't save VFP info, but blanks it out instead.
+     * Instead, sysarch(ARM_GET_VFPSTATE) is used instead.
+     */
+    mcp->mc_vfp_size = 0;
+    mcp->mc_vfp_ptr = 0;
+    memset(&mcp->mc_spare, 0, sizeof(mcp->mc_spare));
+
     return 0;
 }
 
