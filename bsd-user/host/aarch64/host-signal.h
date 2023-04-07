00e0ce41ca8d59a889edc60bd3d5365d70fedbdf
Warner Losh <imp@FreeBSD.org>
2023-04-07 12:13:40 -0600

    bsd-user: Fix aarch64 host signal implementation
    Fix the typos in aarch64 host signal implementation. It now compiles,
    but it's unclear if it works or not.
    
    Signed-off-by: Warner Losh <imp@FreeBSD.org>

--- bsd-user/host/aarch64/host-signal.h
+++ bsd-user/host/aarch64/host-signal.h
@@ -13,12 +13,12 @@
 
 static inline uintptr_t host_signal_pc(ucontext_t *uc)
 {
-    return uc->uc_mcontext.mc_gregs.gp_lr;
+    return uc->uc_mcontext.mc_gpregs.gp_lr;
 }
 
 static inline void host_signal_set_pc(ucontext_t *uc, uintptr_t pc)
 {
-    uc->uc_mcontext.mc_gregs.gp_lr = pc;
+    uc->uc_mcontext.mc_gpregs.gp_lr = pc;
 }
 
 static inline bool host_signal_write(siginfo_t *info, ucontext_t *uc)
