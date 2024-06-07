1970c56fc5ebe4967744aebfb5015c3eb516df71
Warner Losh <imp@bsdimp.com>
2024-06-07 07:51:31 -0600

    bsd-user: Eliminate unused regs arg in load_elf_binary
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/bsdload.c
+++ bsd-user/bsdload.c
@@ -192,7 +192,7 @@ int loader_exec(const char *filename, char **argv, char **envp,
                 && bprm->buf[1] == 'E'
                 && bprm->buf[2] == 'L'
                 && bprm->buf[3] == 'F') {
-            retval = load_elf_binary(bprm, regs, infop);
+            retval = load_elf_binary(bprm, infop);
         } else {
             fprintf(stderr, "Unknown binary format\n");
             retval = -1;
