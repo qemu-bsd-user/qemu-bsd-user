1823534931295f748f9832e596b4cfb8c4e2d7a3
Warner Losh <imp@bsdimp.com>
2026-04-13 08:50:39 -0600

    bsd-user: Fix time_t on amd64
    TARGET_I386 is defined on 64-bit builds too. Fix that, and remove a
    now-noticed duplicated definition.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/freebsd/os-syscall.h
+++ bsd-user/freebsd/os-syscall.h
@@ -12,10 +12,10 @@
  * FreeBSD uses a 64bits time_t except on i386 so we have to add a special case
  * here.
  */
-#if (!defined(TARGET_I386))
-typedef int64_t target_time_t;
-#else
+#if defined(TARGET_I386) && !defined(TARGET_X86_64)
 typedef int32_t target_time_t;
+#else
+typedef int64_t target_time_t;
 #endif
 
 typedef abi_long target_suseconds_t;
