d165c9400e27061df7376a9f3238ae2b74ca586a
Warner Losh <imp@bsdimp.com>
2026-04-20 18:02:33 -0600

    bsd-user: Implement _exit
    Implement exit, since we have to exit gdb as well as tear down any qemu
    plugins.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/freebsd/sys_custom.c
+++ bsd-user/freebsd/sys_custom.c
@@ -1,18 +1,31 @@
 /*
  * process related system call shims and definitions
  *
+ * Copyright (c) 2013-2014 Stacey D. Son
  * Copyright (c) 2026 Warner Losh <imp@bsdimp.com>
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 #include "qemu/osdep.h"
 #include "qemu.h"
+#include "gdbstub/syscalls.h"
+#include "qemu/plugin.h"
 
 #include "special-syscall.h"
 
+#include "os-types.h"
+#include "os-sysproto.h"
+
+/* exit(2) */
 abi_long do_custom__exit(const os_syscall_args_t *sa)
 {
-    return -ENOSYS;
+    struct target__exit_args *uap = (struct target__exit_args *)sa->args;
+
+    gdb_exit(uap->rval);
+    qemu_plugin_user_exit();
+    _exit(uap->rval);
+
+    return 0;
 }
 
 abi_long do_custom_fork(const os_syscall_args_t *sa)
