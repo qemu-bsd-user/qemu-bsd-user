364e76a5367057a77ba877945ba8501f1de28278
Warner Losh <imp@bsdimp.com>
2026-05-22 16:03:22 -0600

    bsd-user: Remove dead shm_open2 forward declaration
    It's not needed, so we should remove it.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/freebsd/os-misc.h
+++ bsd-user/freebsd/os-misc.h
@@ -13,9 +13,6 @@
 #include <sched.h>
 #include <kenv.h>
 
-int shm_open2(const char *path, int flags, mode_t mode, int shmflags,
-    const char *);
-
 /* sched_setparam(2) */
 static inline abi_long do_freebsd_sched_setparam(pid_t pid,
         abi_ulong target_sp_addr)
