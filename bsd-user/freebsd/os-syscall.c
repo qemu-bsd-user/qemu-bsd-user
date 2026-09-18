acf0fe8718012187aba2b6c5ff84c716a0197510
Rick Richard <rick@sloservers.com>
2026-09-18 00:02:35 -0700

    bsd-user: implement posix_fadvise
    Signed-off-by: Rick Richard <rick@sloservers.com>

--- bsd-user/freebsd/os-syscall.c
+++ bsd-user/freebsd/os-syscall.c
@@ -491,6 +491,11 @@ static abi_long freebsd_syscall(CPUArchState *env, int num, abi_long arg1,
         ret = do_bsd_pread(env, arg1, arg2, arg3, arg4, arg5, arg6);
         break;
 
+    case TARGET_FREEBSD_NR_posix_fadvise: /* posix_fadvise(2) */
+        ret = do_bsd_posix_fadvise(env, arg1, arg2, arg3, arg4, arg5, arg6,
+            arg7);
+        break;
+
     case TARGET_FREEBSD_NR_readv: /* readv(2) */
         ret = do_bsd_readv(arg1, arg2, arg3);
         break;
